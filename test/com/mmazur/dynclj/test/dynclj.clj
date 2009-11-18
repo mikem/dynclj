14857a67c7ccb01dada71516dcd1a8cba5fefdfd
Mike Mazur <mmazur@gmail.com>
2009-11-18 21:14:53 +0800

    Refactor tests into individual tests

--- test/com/mmazur/dynclj/test/dynclj.clj
+++ test/com/mmazur/dynclj/test/dynclj.clj
@@ -51,56 +51,61 @@
            (update-needed? {:ip "220.255.77.147" :date "Sun, 01 Nov 2009 21:04:47 SGT"}
                            {:ip "220.255.88.158" :date "Sat, 30 Oct 2009 04:54:23 SGT"})))))
 
-(deftest test-write-cache
-  (testing "Write cache containing one entry"
+(deftest test-write-cache-with-one-entry
+  (is (= cache-with-one-entry-string
+         (do
+           (write-cache cache-with-one-entry cache-test-filename)
+           (slurp cache-test-filename)))))
+
+(deftest test-write-cache-with-one-entry-global-cache-filename
+  (binding [*cache-file-name* cache-test-filename]
     (is (= cache-with-one-entry-string
            (do
-             (write-cache cache-with-one-entry cache-test-filename)
-             (slurp cache-test-filename)))))
-  (testing "Write cache containing one entry, using the global *cache-file-name*"
-    (binding [*cache-file-name* cache-test-filename]
-      (is (= cache-with-one-entry-string
-             (do
-               (write-cache cache-with-one-entry)
-               (slurp cache-test-filename))))))
-  (testing "Write cache containing two entries"
+             (write-cache cache-with-one-entry)
+             (slurp cache-test-filename))))))
+
+(deftest test-write-cache-with-two-entries
+  (is (= cache-with-two-entries-string
+         (do
+           (write-cache cache-with-two-entries cache-test-filename)
+           (slurp cache-test-filename)))))
+
+(deftest test-write-cache-with-two-entries-global-cache-filename
+  (binding [*cache-file-name* cache-test-filename]
     (is (= cache-with-two-entries-string
            (do
-             (write-cache cache-with-two-entries cache-test-filename)
-             (slurp cache-test-filename)))))
-  (testing "Write cache containing two entries, using the global *cache-file-name*"
-    (binding [*cache-file-name* cache-test-filename]
-      (is (= cache-with-two-entries-string
-             (do
-               (write-cache cache-with-two-entries)
-               (slurp cache-test-filename)))))))
+             (write-cache cache-with-two-entries)
+             (slurp cache-test-filename))))))
 
-(deftest test-read-cache
-  (testing "Read cache containing one entry"
+(deftest test-read-cache-with-one-entry
+  (is (= cache-with-one-entry
+         (do
+           (spit cache-test-filename cache-with-one-entry-string)
+           (read-cache cache-test-filename)))))
+
+(deftest test-read-cache-with-one-entry-global-cache-filename
+  (binding [*cache-file-name* cache-test-filename]
     (is (= cache-with-one-entry
            (do
              (spit cache-test-filename cache-with-one-entry-string)
-             (read-cache cache-test-filename)))))
-  (testing "Read cache containing one entry, using the global *cache-file-name*"
-    (binding [*cache-file-name* cache-test-filename]
-      (is (= cache-with-one-entry
-             (do
-               (spit cache-test-filename cache-with-one-entry-string)
-               (read-cache))))))
-  (testing "Read cache containing two entries"
-    (is (= cache-with-two-entries
-           (do
-             (spit cache-test-filename cache-with-two-entries-string)
-             (read-cache cache-test-filename)))))
-  (testing "Read cache containing two entries, using the global *cache-file-name*"
+             (read-cache))))))
+
+(deftest test-read-cache-with-two-entries
+  (is (= cache-with-two-entries
+         (do
+           (spit cache-test-filename cache-with-two-entries-string)
+           (read-cache cache-test-filename)))))
+
+(deftest test-read-cache-with-two-entries-global-cache-filename
     (binding [*cache-file-name* cache-test-filename]
       (is (= cache-with-two-entries
              (do
                (spit cache-test-filename cache-with-two-entries-string)
                (read-cache))))))
-  (testing "Read non-existing cache file"
-    (is (= []
-           (read-cache "/tmp/this-file-does-not-exist")))))
+
+(deftest test-read-cache-with-non-existent-filename
+  (is (= []
+         (read-cache "/tmp/this-file-does-not-exist"))))
 
 (defn my-run-tests
    []
