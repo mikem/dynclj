6db9d4615c47b3c7153aca2c9fcda8d718bf05a0
Mike Mazur <mmazur@gmail.com>
2009-11-23 22:32:33 +0800

    Add function which builds a map from config file

--- src/com/mmazur/dynclj/dynclj.clj
+++ src/com/mmazur/dynclj/dynclj.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:use [clojure.contrib.base64 :only [encode-str]]
         [clojure.contrib.duck-streams :only [file-str writer read-lines]]
+        [clojure.contrib.str-utils :only [re-split]]
         [clojure.http.client :only [request]])
   (:import (java.util Date Calendar)))
 
@@ -21,6 +22,7 @@
 (defn deserialize-date [s]
   (.parse (java.text.SimpleDateFormat. *date-format*) s))
 
+;;; DynDNS logic
 (defn nochg-update-ok? [current last-update]
   (let [cal (Calendar/getInstance)]
     (.setTime cal (deserialize-date last-update))
@@ -37,6 +39,7 @@
         response-body (first (:body-seq response))]
     (re-find #"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" response-body)))
 
+;;; Cache
 (defn write-cache
   ([cache] (write-cache cache *cache-file-name*))
   ([cache filename]
@@ -52,6 +55,18 @@
      (reduce #(conj %1 (read-string %2)) [] (read-lines filename))
      (catch java.io.FileNotFoundException _ []))))
 
+;;; Read config file
+(defn comment-or-blank? [line]
+  (or (if (re-matches #"^#.*" line) true false)
+      (if (re-matches #"^\s*$" line) true false)))
+
+(defn get-config [file]
+  "Returns a lazy seq of lines found in the config file."
+  (reduce #(assoc %1 (first %2) (second %2)) {} (map #(list (keyword (first %)) (second %))
+       (for [line (remove comment-or-blank? (read-lines file))]
+         (re-split #"=" line)))))
+
+;;; Perform the update
 (def username "test")
 (def password "test")
 (def user-pass-base64-encoded (encode-str (apply str username ":" password)))
