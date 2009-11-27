87f0ade14bc1cc6ad7485db02a1da8c271ac3b83
Mike Mazur <mmazur@gmail.com>
2009-11-27 17:39:59 +0800

    Add Leiningen project.clj file

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject dynclj "0.1-alpha"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]])
