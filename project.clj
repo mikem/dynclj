e2a0d936111fe541914d3bbd3c7e0b205e724a11
Mike Mazur <mmazur@gmail.com>
2009-12-12 15:50:07 +0800

    Add lein-nailgun, update clojure dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject dynclj "0.1-alpha"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]])
+                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-nailgun "0.1.0"]])
