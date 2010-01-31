f874553471189b7056f867ca16b14bf44d85d3dc
Mike Mazur <mmazur@gmail.com>
2010-01-31 20:48:32 +0800

    Let's drop down to clojure-1.1.0 for now

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject dynclj "0.1-alpha"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :namespaces [com.mmazur.dynclj.dynclj]
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[lein-nailgun "0.1.0"]])
