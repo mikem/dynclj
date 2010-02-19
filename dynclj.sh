6fc6815ee6b3a0b46060587987aa13d0113ae3fc
Mike Mazur <mmazur@gmail.com>
2010-02-19 19:26:39 +0800

    Clean up launcher, remove :dev-dependencies entry

--- dynclj.sh
+++ dynclj.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-LIBS="$(find -H lib/ -mindepth 2> /dev/null 1 -maxdepth 1 -print0 | tr \\0 \:)"
-CLASSPATH="src/:classes/:$LIBS"
+CLASSPATH="classes/:lib/*:src"
 exec java -client -cp $CLASSPATH com.mmazur.dynclj.dynclj $*
--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :namespaces [com.mmazur.dynclj.dynclj]
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-nailgun "0.1.0"]])
+                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]])
