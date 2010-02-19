2b7eac0da8516b2325c35f4f702a415fc556de59
Mike Mazur <mmazur@gmail.com>
2010-02-19 17:59:00 +0800

    Update project.clj for Clojure 1.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dynclj "0.1-alpha"
   :namespaces [com.mmazur.dynclj.dynclj]
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
                  [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[lein-nailgun "0.1.0"]])
